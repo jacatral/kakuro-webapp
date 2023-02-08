jacatral/kakuro-webapp

commit 4d428f0d0f0cab4da3172e447cfe89e94a945fb3

Refactor links so that random generation of grids redirect to the new link (instead of retaining the random keyword)

src/components/KakuroController.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import GeneratedGrid from '../models/grids/generatedGrid.js';
@@ -11,7 +11,6 @@ const presetPuzzles = [
     defaultPuzzle,
     'basic2',
 ];
-
 
 class KakuroController extends React.Component {
     /**
@@ -67,10 +66,12 @@ class KakuroController extends React.Component {
 
 /**
  * @description Generate grid-data from seed, for keywords it is a file to read
+ * @param {*} props
  * @param {String} seed
- * @returns {String}
+ * @returns {Object}
  */
-function fetchGridData(seed = defaultPuzzle) {
+function fetchGridData(props, seed = defaultPuzzle) {
+    const { cache } = props;
     if (presetPuzzles.includes(seed)) {
         const presetGrid = new PresetGrid(seed);
         return {
@@ -79,28 +80,18 @@ function fetchGridData(seed = defaultPuzzle) {
         };
     }
 
-    // Default to a random 4x4 grid
-    let generatedGrid = new GeneratedGrid();
-
-    // For random seed, generate grid with given size
-    const generateRegex = new RegExp(/^random\d+/gi);
-    if (seed.match(generateRegex)) {
-        const size = Number.parseInt(seed.replace('random', ''));
-        generatedGrid = new GeneratedGrid(size);
-    }
-
+    // Retrieve grid from a given seed
+    const generatedGrid = new GeneratedGrid(cache);
     const grid = generatedGrid.getCells(seed);
-    return {
-        seed: generatedGrid.getSeed(),
-        cells: grid
-    };
+    return grid;
 }
 
 export default (props) => {
     const params = useParams();
+
     const puzzleSeed = params && params.puzzleSeed;
-    const { seed, cells } = fetchGridData(puzzleSeed);
+    const cells = fetchGridData(props, puzzleSeed);
     return (
-        <KakuroController {...props} params={params} key={seed} cells={cells} />
+        <KakuroController {...props} params={params} key={puzzleSeed} cells={cells} />
     )
 };

src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
+import GeneratedGrid from './models/grids/generatedGrid.js';
+import GridCache from './models/gridCache.js';
 import Home from './components/Home.js'
 import KakuroController from './components/KakuroController.js';
 
-function App() {
-    const generatedGrids = Object.entries(localStorage).reduce((gridMap, [key, gridData]) => {
-        const rows = gridData.split('\n');
-        const sampleRow = rows[0].split(',');
-        const gridSize = sampleRow.length - 1;
+const sizes = [4, 8, 16];
+const PRESEED_COUNT = 3;
 
-        gridMap[gridSize] = gridMap[gridSize] || [];
-        gridMap[gridSize].push(key);
-        return gridMap;
-    }, {});
+function App() {
+    const cache = new GridCache(localStorage);
+
+    // Map grids by size, then prepopulate seeds in case there isn't enough seeds
+    const generatedGrids = GridCache.mapGridsBySize(localStorage);
+    for (const size of sizes) {
+        const seeds = generatedGrids[size] || [];
+        while (seeds.length < PRESEED_COUNT) {
+            const grid = new GeneratedGrid(cache, size);
+            grid.generateGrid();
+            const seed = grid.getSeed();
+            seeds.push(seed);
+        }
+    }
+
+    const [grids, setGrids] = useState(generatedGrids);
+
+    // Generate a new seed on the spot & redirect
+    const generatePuzzle = (size = 4) => {
+        const grid = new GeneratedGrid(cache, size);
+        grid.generateGrid();
+        const seed = grid.getSeed();
+        window.location.href = `/puzzle/${seed}`;
+    };
 
     return (
         <BrowserRouter>
@@ -22,19 +41,19 @@ function App() {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/puzzle/basic1">Introductory Puzzle</Link></li>
                     <li><Link to="/puzzle/basic2">Easy 4x4 Puzzle</Link></li>
-                    <li><Link to="/puzzle/random4">Random 4x4 Puzzle</Link></li>
-                    <li><Link to="/puzzle/random8">Random 8x8 Puzzle</Link></li>
-                    <li><Link to="/puzzle/random16">Random 16x16 Puzzle</Link></li>
+                    <li><a onClick={() => generatePuzzle(4)}>Random 4x4 Puzzle</a></li>
+                    <li><a onClick={() => generatePuzzle(8)}>Random 8x8 Puzzle</a></li>
+                    <li><a onClick={() => generatePuzzle(16)}>Random 16x16 Puzzle</a></li>
                 </ul>
             </div>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/puzzle/:puzzleSeed" element={<KakuroController key={window.location.pathname} />}/>
+                <Route path="/puzzle/:puzzleSeed" element={<KakuroController key={window.location.pathname} cache={cache} />}/>
             </Routes>
             <div className="generated-grids">
                 <span>Randomly Generated Grids</span>
                 {
-                    Object.entries(generatedGrids).map(([size, seedList]) => {
+                    Object.entries(grids).map(([size, seedList]) => {
                         return (
                             <div>
                                 <span>{size}</span>

src/models/gridCache.js
@@ -1,0 +1,80 @@
+class GridCache {
+    constructor(gridMap) {
+        this.gridMap = gridMap;
+        this.sizeMap = GridCache.mapGridsBySize(gridMap)
+    }
+
+    /**
+     * @description Group up grid seeds by size
+     * @param {Object<seed, grid>} gridMap
+     * {
+     *   "69b615a9": "-,-,-,11/,8/\n/18,,,,\n-,7/,13/9,,\n/10,,,-,-\n/13,,,,-"
+     * }
+     * @returns {Object}
+     * {
+     *   4: [ "69b615a9" ]
+     * }
+     */
+    static mapGridsBySize(gridMap) {
+        return Object.entries(gridMap).reduce((gridMap, [key, gridData]) => {
+            const rows = gridData.split('\n');
+            const sampleRow = rows[0].split(',');
+            const gridSize = sampleRow.length - 1;
+
+            gridMap[gridSize] = gridMap[gridSize] || [];
+            gridMap[gridSize].push(key);
+            return gridMap;
+        }, {});
+    }
+
+    /**
+     * @decription Retrieve value from the grid-map
+     * @param {String} key
+     * @returns {*}
+     */
+    get(key) {
+        return this.gridMap[key];
+    }
+
+    /**
+     * @description Check if a key exists in the cache
+     * @param {String} key
+     * @returns {Boolean}
+     */
+    contains(key) {
+        return key in this.gridMap;
+    }
+
+    /**
+     * @decription Reverse mapping of a key given the value
+     * @param {String} value
+     * @returns {String}
+     */
+    findKey(value) {
+        for (const [key, gridData] of Object.entries(this.gridMap)) {
+            if (value === gridData) {
+                console.debug('Found grid-data in storage under id:', key);
+                return key;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @decription Update the internal links given a new seed entry
+     * @param {String} key
+     * @param {*} value
+     * @returns {String}
+     */
+    set(key, value) {
+        const sampleRows = value.split('\n');
+        const gridSize = (sampleRows[0].split(',')).length - 1;
+
+        localStorage.setItem(key, value);
+        this.gridMap[key] = value;
+        this.sizeMap[gridSize] = this.sizeMap[gridSize] || [];
+        this.sizeMap[gridSize].push(key);
+    }
+}
+
+export default GridCache;

src/models/grids/generatedGrid.js
@@ -10,19 +10,12 @@ class GeneratedGrid extends Grid {
     /**
      * @param {Number} size [4, 8, 16]
      */
-    constructor(size = 4) {
+    constructor(cache, size = 4) {
         super();
+        this._cache = cache;
         this._size = size;
         this.cells = [];
         this.solution = [];
-    }
-
-    /**
-     * @decription Verify that an input seed corresponds to a locally-stored grid
-     * @returns {Boolean}
-     */
-    checkSeed(key) {
-        return key in localStorage;
     }
 
     /**
@@ -51,11 +44,12 @@ class GeneratedGrid extends Grid {
      * @returns {Array<Array>}
      */
     getCells(seed) {
-        if (this.checkSeed(seed)) {
-            const gridData = localStorage.getItem(seed);
+        if (this._cache.contains(seed)) {
+            const gridData = this._cache.get(seed);
             this.cells = super.parseGridData(gridData);
             return this.cells;
         }
+        console.warn(`Seed, ${seed}, was not found; generating a random 4x4`);
         return this.generateGrid();
     }
 
@@ -282,18 +276,14 @@ class GeneratedGrid extends Grid {
      */
     generateKey(gridData) {
         // Check local storage if the grid was already saved
-        const savedGrids = Object.entries(localStorage);
-        for (const [key, value] of savedGrids) {
-            console.log(gridData, key, value)
-            if (gridData === value) {
-                console.log('Found grid-data in storage under id:', key);
-                return key;
-            }
+        const existingKey = this._cache.findKey(gridData);
+        if (existingKey) {
+            return this._cache.get(existingKey);
         }
 
         // Assign an id for a new grid & store locally
         const newKey = uuid().split('-')[0];
-        localStorage.setItem(newKey, gridData);
+        this._cache.set(newKey, gridData);
         return newKey;
     }
 

commit 0aece64e13f30beb213654cfddcfe36cb3ef9a76

Fixed regression in pages to render preset grids (caused by 4d428f0)

src/components/KakuroController.js
@@ -74,10 +74,7 @@ function fetchGridData(props, seed = defaultPuzzle) {
     const { cache } = props;
     if (presetPuzzles.includes(seed)) {
         const presetGrid = new PresetGrid(seed);
-        return {
-            seed: seed,
-            cells: presetGrid.getCells()
-        };
+        return presetGrid.getCells();
     }
 
     // Retrieve grid from a given seed